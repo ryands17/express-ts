1e97a4e713c122137cc6a9867d1ec051e9b0e632
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2019-02-03 01:55:52 +0530

    fix typos

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import { factorial } from 'utils/factorial'
 
 const app = express()
 
-app.listen(3000, () => console.log(`app running at port 3000`))
+app.listen(3000, () => console.log(`app running on port 3000`))
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   let f = factorial(10)
-  res.send('TS app live!')
+  res.send(`10! = ${f}`)
 })
