6889105f20f42f9705fc5cd1797a995180d9b888
Ryan Dsouza <dsouza.ryan.11@gmail.com>
2019-02-03 02:13:34 +0530

    add .env support via dotenv and helmet for secure headers

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
+import './config'
 import express from 'express'
+import helmet from 'helmet'
 import { factorial } from 'utils/factorial'
 
 const app = express()
+const PORT = Number(process.env.PORT)
+app.use(helmet())
 
-app.listen(3000, () => console.log(`app running on port 3000`))
+app.listen(PORT, () => console.log(`app running on port ${PORT}`))
 
 app.get('/', (_, res) => {
   let f = factorial(10)
